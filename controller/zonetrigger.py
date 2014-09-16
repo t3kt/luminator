653cac5d0821d1a4ea50bd4b7252ecb735e0826b
t3kt <tommy.etkin@gmail.com>
2014-09-15 21:06:10 -0400

    fixed bug in zone bounds handling

--- controller/zonetrigger.py
+++ controller/zonetrigger.py
@@ -3,9 +3,9 @@ def cook(scriptOP):
 	tracked, x, z = invals.chan('*_tracked')[0], invals.chan('*:tx')[0], invals.chan('*:tz')[0]
 	scriptOP.clear()
 	zonetbl = op('zones')
-	xbounds = float(zonetbl[i, 'xmin']), float(zonetbl[i, 'xmax'])
-	zbounds = float(zonetbl[i, 'zmin']), float(zonetbl[i, 'zmax'])
 	for i in range(1, zonetbl.numRows):
+		xbounds = float(zonetbl[i, 'xmin']), float(zonetbl[i, 'xmax'])
+		zbounds = float(zonetbl[i, 'zmin']), float(zonetbl[i, 'zmax'])
 		ch = scriptOP.appendChan(zonetbl[i, 'event'])
 		if tracked and xbounds[0] <= x < xbounds and zbounds[0] <= z < zbounds[1]:
 			ch.vals = [1]
