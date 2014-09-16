69babd58874e5d8e0f4595307caa173a73cf5d7b
t3kt <tommy.etkin@gmail.com>
2014-09-15 20:59:53 -0400

    work on the kinect input selection, adapted zone-based input to 3d points (fixes #16), starting work on input rotation (see #17)

--- controller/zonetrigger.py
+++ controller/zonetrigger.py
@@ -1,11 +1,13 @@
 def cook(scriptOP):
 	invals = scriptOP.inputs[0]
-	tracked, u, v = invals.chan('*:tracked')[0], invals.chan('*:u')[0], invals.chan('*:v')[0]
+	tracked, x, z = invals.chan('*_tracked')[0], invals.chan('*:tx')[0], invals.chan('*:tz')[0]
 	scriptOP.clear()
 	zonetbl = op('zones')
+	xbounds = float(zonetbl[i, 'xmin']), float(zonetbl[i, 'xmax'])
+	zbounds = float(zonetbl[i, 'zmin']), float(zonetbl[i, 'zmax'])
 	for i in range(1, zonetbl.numRows):
 		ch = scriptOP.appendChan(zonetbl[i, 'event'])
-		if tracked and float(zonetbl[i, 'umin'].val) <= u < float(zonetbl[i, 'umax'].val) and float(zonetbl[i, 'vmin'].val) <= v < float(zonetbl[i, 'vmax'].val):
+		if tracked and xbounds[0] <= x < xbounds and zbounds[0] <= z < zbounds[1]:
 			ch.vals = [1]
 		else:
 			ch.vals = [0]
