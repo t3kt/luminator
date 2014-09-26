726a7601d320437c0be24596730e6a940fff04ec
t3kt <tommy.etkin@gmail.com>
2014-09-26 19:39:05 -0400

    implemented player active/inactive detection (closes #27), starting to implement cell-based stuff (see #28)

---
+++ controller/get_players_active.py
@@ -0,0 +1,18 @@
+def cook(scriptOP):
+	scriptOP.clear()
+	statuses = scriptOP.inputs[0]
+	for playerCell in op('players').row(0):
+		player = playerCell.val
+		active = is_player_active(statuses, player)
+		ch = scriptOP.appendChan(player+'/active')
+		ch.vals = [1 if active else 0]
+
+def is_player_active(statuses, player):
+	level  = get_tracked(statuses, player, 'spine')
+	level += get_tracked(statuses, player, 'hand_l')
+	level += get_tracked(statuses, player, 'hand_r')
+	return level > 3
+
+def get_tracked(statuses, player, part):
+	ch = statuses[player+'/'+part+'_tracked']
+	return ch[0] if ch else 0
