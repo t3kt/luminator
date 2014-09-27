8d7fa571a164d7ecbe47c89bd0f30056fd76bbde
t3kt <tommy.etkin@gmail.com>
2014-09-27 02:16:30 -0400

    probably fixed #32 and fixed #32 by not using local variables and some other tweaks (see also #28)

--- controller/get_cell_active_points.py
+++ controller/get_cell_active_points.py
@@ -6,7 +6,6 @@ def cook(chop):
 	player = playertbl[0,0].val
 	inpfx = player + '/hand_'
 	inchop = chop.inputs[0]
-	outpfx = chop.var('id') + '/hand_'
 	for part in ['l:tx','l:ty','l:tz',
 				 'r:tx','r:ty','r:tz']:
-		chop.appendChan(outpfx+part)[0] = inchop[inpfx+part]
+		chop.appendChan('hand_'+part)[0] = inchop[inpfx+part]
