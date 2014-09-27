329c17f408580c49a9104afa0073aa87af128824
t3kt <tommy.etkin@gmail.com>
2014-09-27 16:51:03 -0400

    added cell center coordinates (see #28)

--- controller/pre_process_cells.py
+++ controller/pre_process_cells.py
@@ -3,16 +3,19 @@ def cook(dat):
 	cellsin = dat.inputs[0]
 	numx = max(cellsin.col('cellx')[1:])
 	numz = max(cellsin.col('cellz')[1:])
-	dat.appendRow(cellsin.row(0)+ ['xmin', 'xmax', 'zmin', 'zmax'])
-	bounds = op('kinectbounds')
+	dat.appendRow(cellsin.row(0) + ['xmin', 'xmax', 'zmin', 'zmax', 'centerx', 'centerz'])
 	xbounds = float(dat.var('kinectscaledxmin')), float(dat.var('kinectscaledxmax'))
 	zbounds = float(dat.var('kinectscaledzmin')), float(dat.var('kinectscaledzmax'))
 	for i in range(1, cellsin.numRows):
 		x, z = int(cellsin[i, 'cellx']), int(cellsin[i, 'cellz'])
 		parts = cellsin.row(i)
-		parts += [ scale(x, (0, numx), xbounds), scale(x+1, (0, numx), xbounds) ]
-		parts += [ scale(z, (0, numz), zbounds), scale(z+1, (0, numz), zbounds) ]
+		xmin, xmax = scale(x, numx, xbounds), scale(x + 1, numx, xbounds)
+		parts += [xmin, xmax]
+		zmin, zmax = scale(z, numz, zbounds), scale(z + 1, numz, zbounds)
+		parts += [zmin, zmax]
+		center = (xmin + xmax) / 2.0, (zmin + zmax) / 2.0
+		parts += center
 		dat.appendRow(parts)
 
-def scale(x, inrange, outrange):
-	return (x-inrange[0])*(outrange[1]-outrange[0])/(inrange[1] - inrange[0]) + outrange[0]
+def scale(x, inmax, outrange):
+	return x*(outrange[1]-outrange[0])/inmax + outrange[0]
