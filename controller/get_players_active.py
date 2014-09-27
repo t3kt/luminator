cee7fc4d9c20b7d0eeb2f785ebb5bbb20cd3a7c2
t3kt <tommy.etkin@gmail.com>
2014-09-27 00:37:30 -0400

    implemented cell active player counts (see #28)

--- controller/get_players_active.py
+++ controller/get_players_active.py
@@ -1,7 +1,7 @@
 def cook(scriptOP):
 	scriptOP.clear()
 	statuses = scriptOP.inputs[0]
-	for playerCell in op('players').col(0):
+	for playerCell in op('sel_players').col(0):
 		player = playerCell.val
 		active = is_player_active(statuses, player)
 		ch = scriptOP.appendChan(player)
