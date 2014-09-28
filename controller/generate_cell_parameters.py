485c4820e61b0275df1507ff29e998bddbafba33
t3kt <tommy.etkin@gmail.com>
2014-09-27 20:18:23 -0400

    scale coordinates to cell bounds (re) closes #33

--- controller/generate_cell_parameters.py
+++ controller/generate_cell_parameters.py
@@ -4,7 +4,10 @@ def cook(scriptOP):
 	handsin = scriptOP.inputs[1]
 	activecount = 0
 	firstactive = 0
-	centerx, centerz = scriptOP.par.value0x, scriptOP.par.value0z
+	xbounds = scriptOP.par.value0x, scriptOP.par.value1x
+	zbounds = scriptOP.par.value0z, scriptOP.par.value1z
+	xzoutbounds = -1, 1
+	ybounds = 0, 1
 	playername = None
 	for i in range(playersin.numChans):
 		playerch = playersin[i]
@@ -25,12 +28,15 @@ def cook(scriptOP):
 		addchan(scriptOP, 'hand_r:ty', 0)
 		addchan(scriptOP, 'hand_r:tz', 0)
 	else:
-		addchan(scriptOP, 'hand_l:tx', handsin[playername + '/hand_l:tx'] - centerx)
-		addchan(scriptOP, 'hand_l:ty', handsin[playername + '/hand_l:ty'])
-		addchan(scriptOP, 'hand_l:tz', handsin[playername + '/hand_l:tz'] - centerz)
-		addchan(scriptOP, 'hand_r:tx', handsin[playername + '/hand_r:tx'] - centerx)
-		addchan(scriptOP, 'hand_r:ty', handsin[playername + '/hand_r:ty'])
-		addchan(scriptOP, 'hand_r:tz', handsin[playername + '/hand_r:tz'] - centerz)
+		addchan(scriptOP, 'hand_l:tx', handsin[playername + '/hand_l:tx'], xbounds, xzoutbounds)
+		addchan(scriptOP, 'hand_l:ty', handsin[playername + '/hand_l:ty'], ybounds, ybounds)
+		addchan(scriptOP, 'hand_l:tz', handsin[playername + '/hand_l:tz'], zbounds, xzoutbounds)
+		addchan(scriptOP, 'hand_r:tx', handsin[playername + '/hand_r:tx'], xbounds, xzoutbounds)
+		addchan(scriptOP, 'hand_r:ty', handsin[playername + '/hand_r:ty'], ybounds, ybounds)
+		addchan(scriptOP, 'hand_r:tz', handsin[playername + '/hand_r:tz'], zbounds, xzoutbounds)
 
-def addchan(chop, name, val):
+def addchan(chop, name, val, inrange, outrange):
 	chop.appendChan(name)[0] = val
+
+def scale(x, inrange, outrange):
+	return (x-inrange[0])*(outrange[1]-outrange[0])/(inrange[1] - inrange[0]) + outrange[0]
