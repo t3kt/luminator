52b581d80876b1a2cbc80cd26ea72354d495dcd3
t3kt <tommy.etkin@gmail.com>
2014-10-01 20:53:23 -0400

    switching to arbitrarily positioned cells, with support for debug renderer (#42)

--- scripts/pre_process_cells.py
+++ scripts/pre_process_cells.py
@@ -1,20 +1,13 @@
 def cook(dat):
 	dat.clear()
 	cellsin = dat.inputs[0]
-	numx = max(cellsin.col('cellx')[1:])
-	numz = max(cellsin.col('cellz')[1:])
-	dat.appendRow(cellsin.row(0) + ['xmin', 'xmax', 'zmin', 'zmax', 'centerx', 'centerz'])
-	xbounds = float(dat.var('kinectscaledxmin')), float(dat.var('kinectscaledxmax'))
-	zbounds = float(dat.var('kinectscaledzmin')), float(dat.var('kinectscaledzmax'))
+	dat.appendRow(cellsin.row(0) + ['centerx', 'centerz', 'sizex', 'sizez'])
 	for i in range(1, cellsin.numRows):
-		x, z = int(cellsin[i, 'cellx']), int(cellsin[i, 'cellz'])
 		parts = cellsin.row(i)
-		xmin, xmax = scale(x, numx, xbounds), scale(x + 1, numx, xbounds)
-		parts += [xmin, xmax]
-		zmin, zmax = scale(z, numz, zbounds), scale(z + 1, numz, zbounds)
-		parts += [zmin, zmax]
-		center = (xmin + xmax) / 2.0, (zmin + zmax) / 2.0
-		parts += center
+		xmin, xmax = cellsin[i, 'xmin'], cellsin[i, 'xmax']
+		zmin, zmax = cellsin[i, 'zmin'], cellsin[i, 'zmax']
+		parts += [(xmin + xmax) / 2.0, (zmin + zmax) / 2.0]
+		parts += [(xmax - xmin), (zmax - zmin)]
 		dat.appendRow(parts)
 
 def scale(x, inmax, outrange):
