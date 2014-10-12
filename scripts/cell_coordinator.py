4d809834a3e0559a8553c2a054ce814a46fd4bb4
t3kt <tommy.etkin@gmail.com>
2014-10-12 19:02:50 -0400

    initial implementation of cell following is almost done (#50)

--- scripts/cell_coordinator.py
+++ scripts/cell_coordinator.py
@@ -19,21 +19,62 @@ def init(cell_defs, cell_states):
         cell_states[i, 'sizez'] = cell_defs[i, 'sizez']
         cell_states[i, 'active'] = 0
         cell_states[i, 'player'] = ''
+
+def update(cell_states, points, players_active):
+    for cell_index in range(1, cell_states.numRows):
+        cell_id = cell_states[cell_index, 'id'].val
+        active = cell_states[cell_index, 'active'] == '1'
+        if active:
+            player_id = cell_states[cell_index, 'player']
+            if not players_active[player_id][0]:
+                deactivate_cell(cell_states, cell_id)
+                update_inactive_cell(cell_states, cell_index, points)
+            else:
+                update_active_cell(cell_states, cell_index, points)
+        else:
+            player_id = get_player_in_cell(points, players_active, cell_states, cell_index)
+            if player_id:
+                activate_cell(cell_states, cell_index, points, player_id)
+                update_active_cell(cell_states, cell_index, points)
+            else:
+                update_inactive_cell(cell_states, cell_index, points)
+
+def get_player_in_cell(points, players_active, cell_states, cell_index):
+    xmin, xmax = get_dat_value_pair(cell_states, cell_index, 'rest_xmin', 'rest_xmax')
+    zmin, zmax = get_dat_value_pair(cell_states, cell_index, 'rest_zmin', 'rest_zmax')
+    for player in players_active.chans():
+        if not player[0]:
+            continue
+        px, pz = get_player_position(points, player.name)
+        if xmin <= px < xmax and zmin <= pz < zmax:
+            return player.name
+
+def get_dat_value_pair(dat, row, col_a, col_b):
+    return float(dat[row, col_a]), float(dat[row, col_b])
+
+def get_player_position(points, player_id):
+    return points[player_id + '/spine:tx'][0], points[player_id + '/spine:tz'][0]
+
+def activate_cell(cell_states, cell_index, points, player_id):
+    cell_states[cell_index, 'player'] = player_id
+    cell_states[cell_index, 'active'] = 1
     pass
 
-def update(points, players, cell_states):
-    for i in range(1, cell_states.numRows):
-        id = cell_states[i, 'id'].val
-        active = cell_states[i, 'active'] == '1'
-        pass
+def update_active_cell(cell_states, cell_index, points):
+    player_id = cell_states[cell_index, 'player']
+    px, pz = get_player_position(points, player_id)
+    sizex, sizez = get_dat_value_pair(cell_states, cell_index, 'sizex', 'sizez')
+    cell_states[cell_index, 'centerx'], cell_states[cell_index, 'centerz'] = px, pz
+    cell_states[cell_index, 'xmin'], cell_states[cell_index, 'xmax'] = px - (sizex / 2), px + (sizex / 2)
+    cell_states[cell_index, 'zmin'], cell_states[cell_index, 'zmax'] = pz - (sizez / 2), pz + (sizez / 2)
     pass
 
-def update_active_cell(points, players, cell_states, id):
-    pass
+def deactivate_cell(cell_states, cell_index):
+    cell_states[cell_index, 'player'] = ''
+    cell_states[cell_index, 'active'] = 0
+    for part in ['xmin', 'xmax', 'zmin', 'zmax', 'centerx', 'centerz']:
+        cell_states[cell_index, part] = cell_states[cell_index, 'rest_' + part]
 
-def deactivate_cell(cell_states, id):
-    pass
-
-def activate_cell(cell_states, id, points, player):
+def update_inactive_cell(cell_states, cell_index, points):
     pass
 
