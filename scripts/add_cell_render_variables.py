2be872a9bcd06355bbdc1a40966505826e927142
t3kt <tommy.etkin@gmail.com>
2014-10-15 22:55:38 -0400

    removed redundant script

--- scripts/add_cell_render_variables.py
+++ scripts/add_cell_render_variables.py
@@ -8,14 +8,15 @@ def cook(dat):
 	gridw = gridxrange[1] - gridxrange[0]
 	gridh = gridzrange[1] - gridzrange[0]
 	renderw, renderh = float(var('renderw')), float(var('renderh'))
+	renderwrange, renderhrange = [-renderw / 2, renderw / 2], [renderh / 2, -renderh / 2]
 	for i in range(1, cellsin.numRows):
 		sizex, sizez = float(celldefs[i, 'sizex']), float(celldefs[i, 'sizez'])
 		centerx, centerz = float(cellsin[i, 'centerx']), float(cellsin[i, 'centerz'])
 		cellrw = int(scale(sizex, [0, gridw], [0, renderw]))
 		cellrh = int(scale(sizez, [0, gridh], [0, renderh]))
-		celltx = int(scale(centerx, gridxrange, [-renderw/2, renderw/2]))
-		cellty = int(scale(centerz, gridzrange, [renderh/2, -renderh/2]))
-		dat.appendRow(cellsin.row(i)+[cellrw, cellrh, celltx, cellty])
+		celltx = int(scale(centerx, gridxrange, renderwrange))
+		cellty = int(scale(centerz, gridzrange, renderhrange))
+		dat.appendRow(cellsin.row(i) + [cellrw, cellrh, celltx, cellty])
 
 def scale(x, inrange, outrange):
-	return (x-inrange[0])*(outrange[1]-outrange[0])/(inrange[1] - inrange[0]) + outrange[0]
+	return (x - inrange[0]) * (outrange[1] - outrange[0])/(inrange[1] - inrange[0]) + outrange[0]
