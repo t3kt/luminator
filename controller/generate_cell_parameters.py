5c9c5cee229a11618116ff080e15a3f5cd560f76
t3kt <tommy.etkin@gmail.com>
2014-10-15 20:01:25 -0400

    lots of optimizations, removed unused rest values from generated cell states

--- controller/generate_cell_parameters.py
+++ controller/generate_cell_parameters.py
@@ -12,24 +12,16 @@ def cook(scriptOP):
 	active = playername is not None and playername != ''
 	scriptOP.appendChan('active')[0] = active
 	if not active:
-		addchan(scriptOP, 'hand_l:tx', 0)
-		addchan(scriptOP, 'hand_l:ty', 0)
-		addchan(scriptOP, 'hand_l:tz', 0)
-		addchan(scriptOP, 'hand_r:tx', 0)
-		addchan(scriptOP, 'hand_r:ty', 0)
-		addchan(scriptOP, 'hand_r:tz', 0)
-	else:
-		addchan(scriptOP, 'hand_l:tx', handsin[playername + '/hand_l:tx'], xbounds, outbounds)
-		addchan(scriptOP, 'hand_l:ty', handsin[playername + '/hand_l:ty'], ybounds, outbounds)
-		addchan(scriptOP, 'hand_l:tz', handsin[playername + '/hand_l:tz'], zbounds, outbounds)
-		addchan(scriptOP, 'hand_r:tx', handsin[playername + '/hand_r:tx'], xbounds, outbounds)
-		addchan(scriptOP, 'hand_r:ty', handsin[playername + '/hand_r:ty'], ybounds, outbounds)
-		addchan(scriptOP, 'hand_r:tz', handsin[playername + '/hand_r:tz'], zbounds, outbounds)
+		return
+	addchan(scriptOP, 'hand_l:tx', handsin[playername + '/hand_l:tx'], xbounds, outbounds)
+	addchan(scriptOP, 'hand_l:ty', handsin[playername + '/hand_l:ty'], ybounds, outbounds)
+	addchan(scriptOP, 'hand_l:tz', handsin[playername + '/hand_l:tz'], zbounds, outbounds)
+	addchan(scriptOP, 'hand_r:tx', handsin[playername + '/hand_r:tx'], xbounds, outbounds)
+	addchan(scriptOP, 'hand_r:ty', handsin[playername + '/hand_r:ty'], ybounds, outbounds)
+	addchan(scriptOP, 'hand_r:tz', handsin[playername + '/hand_r:tz'], zbounds, outbounds)
 
-def addchan(chop, name, val, inrange = None, outrange = None):
-	if inrange is not None:
-		val = scale(val, inrange, outrange)
-	chop.appendChan(name)[0] = val
+def addchan(chop, name, val, inrange, outrange):
+	chop.appendChan(name)[0] = scale(val, inrange, outrange)
 
 def scale(x, inrange, outrange):
 	return (x-inrange[0])*(outrange[1]-outrange[0])/(inrange[1] - inrange[0]) + outrange[0]
--- scripts/cell_coordinator.py
+++ scripts/cell_coordinator.py
@@ -1,47 +1,43 @@
 def init(cell_defs, cell_states):
     cell_states.clear()
     cell_states.appendRow(['id', 'active', 'player',
-                           'rest_centerx', 'rest_centerz',
-                           'rest_xmin', 'rest_xmax', 'rest_zmin', 'rest_zmax',
-                           'centerx', 'centerz',
-                           'xmin', 'xmax', 'zmin', 'zmax',
-                           'sizex', 'sizez'])
+                           'centerx', 'centerz', 'xmin', 'xmax', 'zmin', 'zmax', 'sizex', 'sizez'])
     for i in range(1, cell_defs.numRows):
         cell_states.appendRow()
         cell_states[i, 'id'] = cell_defs[i, 'id']
-        cell_states[i, 'rest_centerx'] = cell_states[i, 'centerx'] = cell_defs[i, 'centerx']
-        cell_states[i, 'rest_centerz'] = cell_states[i, 'centerz'] = cell_defs[i, 'centerz']
-        cell_states[i, 'rest_xmin'] = cell_states[i, 'xmin'] = cell_defs[i, 'xmin']
-        cell_states[i, 'rest_xmax'] = cell_states[i, 'xmax'] = cell_defs[i, 'xmax']
-        cell_states[i, 'rest_zmin'] = cell_states[i, 'zmin'] = cell_defs[i, 'zmin']
-        cell_states[i, 'rest_zmax'] = cell_states[i, 'zmax'] = cell_defs[i, 'zmax']
+        cell_states[i, 'centerx'] = cell_defs[i, 'centerx']
+        cell_states[i, 'centerz'] = cell_defs[i, 'centerz']
+        cell_states[i, 'xmin'] = cell_defs[i, 'xmin']
+        cell_states[i, 'xmax'] = cell_defs[i, 'xmax']
+        cell_states[i, 'zmin'] = cell_defs[i, 'zmin']
+        cell_states[i, 'zmax'] = cell_defs[i, 'zmax']
         cell_states[i, 'sizex'] = cell_defs[i, 'sizex']
         cell_states[i, 'sizez'] = cell_defs[i, 'sizez']
         cell_states[i, 'active'] = 0
         cell_states[i, 'player'] = ''
 
-def update(cell_states, points, players_active):
+def update(cell_defs, cell_states, points, players_active):
     for cell_index in range(1, cell_states.numRows):
         cell_id = cell_states[cell_index, 'id'].val
         active = cell_states[cell_index, 'active'] == '1'
         if active:
             player_id = cell_states[cell_index, 'player']
             if not players_active[player_id][0]:
-                deactivate_cell(cell_states, cell_id)
-                update_inactive_cell(cell_states, cell_index, points)
+                deactivate_cell(cell_defs, cell_states, cell_id)
+                update_inactive_cell(cell_defs, cell_states, cell_index, points)
             else:
-                update_active_cell(cell_states, cell_index, points)
+                update_active_cell(cell_defs, cell_states, cell_index, points)
         else:
-            player_id = get_player_in_cell(points, players_active, cell_states, cell_index)
+            player_id = get_player_in_cell(points, players_active, cell_defs, cell_states, cell_index)
             if player_id:
-                activate_cell(cell_states, cell_index, points, player_id)
-                update_active_cell(cell_states, cell_index, points)
+                activate_cell(cell_defs, cell_states, cell_index, points, player_id)
+                update_active_cell(cell_defs, cell_states, cell_index, points)
             else:
-                update_inactive_cell(cell_states, cell_index, points)
+                update_inactive_cell(cell_defs, cell_states, cell_index, points)
 
-def get_player_in_cell(points, players_active, cell_states, cell_index):
-    xmin, xmax = get_dat_value_pair(cell_states, cell_index, 'rest_xmin', 'rest_xmax')
-    zmin, zmax = get_dat_value_pair(cell_states, cell_index, 'rest_zmin', 'rest_zmax')
+def get_player_in_cell(points, players_active, cell_defs, cell_states, cell_index):
+    xmin, xmax = float(cell_defs[cell_index, 'xmin']), float(cell_defs[cell_index, 'xmax'])
+    zmin, zmax = float(cell_defs[cell_index, 'zmin']), float(cell_defs[cell_index, 'zmax'])
     for player in players_active.chans():
         if not player[0]:
             continue
@@ -55,26 +51,26 @@ def get_dat_value_pair(dat, row, col_a, col_b):
 def get_player_position(points, player_id):
     return points[player_id + '/spine:tx'][0], points[player_id + '/spine:tz'][0]
 
-def activate_cell(cell_states, cell_index, points, player_id):
+def activate_cell(cell_defs, cell_states, cell_index, points, player_id):
     cell_states[cell_index, 'player'] = player_id
     cell_states[cell_index, 'active'] = 1
     pass
 
-def update_active_cell(cell_states, cell_index, points):
+def update_active_cell(cell_defs, cell_states, cell_index, points):
     player_id = cell_states[cell_index, 'player']
     px, pz = get_player_position(points, player_id)
-    sizex, sizez = get_dat_value_pair(cell_states, cell_index, 'sizex', 'sizez')
+    sizex, sizez = get_dat_value_pair(cell_defs, cell_index, 'sizex', 'sizez')
     cell_states[cell_index, 'centerx'], cell_states[cell_index, 'centerz'] = px, pz
     cell_states[cell_index, 'xmin'], cell_states[cell_index, 'xmax'] = px - (sizex / 2), px + (sizex / 2)
     cell_states[cell_index, 'zmin'], cell_states[cell_index, 'zmax'] = pz - (sizez / 2), pz + (sizez / 2)
     pass
 
-def deactivate_cell(cell_states, cell_index):
+def deactivate_cell(cell_defs, cell_states, cell_index):
     cell_states[cell_index, 'player'] = ''
     cell_states[cell_index, 'active'] = 0
     for part in ['xmin', 'xmax', 'zmin', 'zmax', 'centerx', 'centerz']:
-        cell_states[cell_index, part] = cell_states[cell_index, 'rest_' + part]
+        cell_states[cell_index, part] = cell_defs[cell_index, part]
 
-def update_inactive_cell(cell_states, cell_index, points):
+def update_inactive_cell(cell_defs, cell_states, cell_index, points):
     pass
 
